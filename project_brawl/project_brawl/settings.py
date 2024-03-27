8d2e1a9220d511ed250a08ebe95b586a902ea1d2
Kelvin Feng <81874388+kelvinos2624@users.noreply.github.com>
2024-03-26 23:44:14 -0400

    CSS implementation
    implemented CSS file

--- project_brawl/project_brawl/settings.py
+++ project_brawl/project_brawl/settings.py
@@ -119,7 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Additional directories to collect static files from
+STATICFILES_DIRS = [
+    BASE_DIR / "static/css",
+]
 
+# Directory to collect static files for deployment
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- project_brawl/tracker_app/views.py
+++ project_brawl/tracker_app/views.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv, dotenv_values
 load_dotenv()
 
 def authenticate(player):
-    api_key = "YOUR_API_KEY_HERE"
     url = f"https://api.brawlstars.com/v1/players/%23{player}"
     headers = {
         "Authorization": "Bearer " + os.getenv("API_KEY")
@@ -21,12 +20,13 @@ def find_tilted_brawlers(response):
     return dict(sorted(tilt.items(), key=lambda x: x[1], reverse=True))
 
 def menu(request):
-    player_id = request.GET.get('player_id')
-    if player_id:
-        response = authenticate(player_id)
-        converted_tilt = find_tilted_brawlers(response)
-        return render(request, 'menu.html', {'converted_tilt': converted_tilt, 'player_name': response.json()["name"]})
-    else:
+    try:
+        player_id = request.GET.get('player_id')
+        if player_id:
+            response = authenticate(player_id)
+            converted_tilt = find_tilted_brawlers(response)
+            return render(request, 'menu.html', {'converted_tilt': converted_tilt, 'player_name': response.json()["name"]})
+    except:
         return render(request, 'error.html', {'message': 'Player ID not provided'})
 
 def index(request):

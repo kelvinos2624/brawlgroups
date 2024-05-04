d2c7fee0e7a930093c9882a2815eff518b83ee27
kelvinos <kelvinfeng05@gmail.com>
2024-05-04 01:28:13 -0400

    setup

--- project_brawl/project_brawl/settings.py
+++ project_brawl/project_brawl/settings.py
@@ -49,7 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
 ROOT_URLCONF = 'project_brawl.urls'
 
